9fc811680b3a51756c2772f69d9340f04390b1ef
Dorian Gouzou <32414502+jackred@users.noreply.github.com>
2021-10-01 00:07:22 +0100

    FIX semicolon
    god bless editing directly on github

--- utils.js
+++ utils.js
@@ -5,6 +5,6 @@
  */
 exports.log_to_console = (message) => {
   let current_date = new Date();
-  let datetime = ('['+current_date.toLocaleString('fr')+'] ').replace(',', ' @')
+  let datetime = ('['+current_date.toLocaleString('fr')+'] ').replace(',', ' @');
   console.log(datetime.toString() + message);
 }
